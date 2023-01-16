50b9f6d08b4bb78e34bccff8ecac137d357dbd0a
G-RIMA <gikandimaria178@gmail.com>
2023-01-16 13:17:35 +0300

    removed module

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 '''minimal operations interview '''
-import sys
 
 
 def minOperations(n: int) -> int:
     """ Minimum Operations """
 
-    dp = [sys.maxsize for i in range(n + 1)]
+    dp = [n > 1 for i in range(n + 1)]
     dp[0] = 0
     for i in range(1, n + 1):
         dp[i] = i
