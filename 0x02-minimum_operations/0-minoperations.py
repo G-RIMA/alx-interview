1d6cfe0a5b0bdfc4fc465040633c5068256d1199
G-RIMA <gikandimaria178@gmail.com>
2023-01-16 13:00:30 +0300

    minimal operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+
+def minOperations(n: int) -> int:
+    """ Minimum Operations """
+
+    dp = [sys.maxsize for i in range(n + 1)]
+    dp[0] = 0
+    for i in range(1, n + 1):
+        dp[i] = i
+        for j in range(2, i):
+            if i % j == 0:
+                dp[i] = min(dp[i], dp[i//j] + j)
+    return dp[n]
